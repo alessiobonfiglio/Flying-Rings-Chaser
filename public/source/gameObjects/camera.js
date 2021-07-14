857b45ada74149f665cc4bc84cebbd90d464f318
Alessio Bonfiglio <alessio.bonfiglio@mail.polimi.it>
2021-07-14 14:18:33 +0200

    basic improvement

--- public/source/gameObjects/camera.js
+++ public/source/gameObjects/camera.js
@@ -1,5 +1,5 @@
 import {default as utils} from "../utils.js"
-import { default as MathUtils } from "../math_utils.js"
+import {default as MathUtils} from "../math_utils.js"
 import GameObject from "./gameObject.js";
 import CancellationToken from "../utils/cancellationToken.js";
 import Animations from "../utils/animations.js";
@@ -23,7 +23,7 @@ class Camera extends GameObject {
 		super();
 		this.position = position;
 		this.horizontalAngle = horizontalAngle;
-		this.verticalAngle = verticalAngle;		
+		this.verticalAngle = verticalAngle;
 		this.#zPos = this.#startZPos;
 		this.fov = this.#startFov;
 	}
@@ -37,9 +37,9 @@ class Camera extends GameObject {
 	}
 
 	update() {
-		if(!this.#animatingPosition)
+		if (!this.#animatingPosition)
 			this.position = this.#computeCurrentPosition();
-	}	
+	}
 
 	async boost(speedUpTime, maintainingTime, slowDownTime) {
 		// Aborting previous animation if any
@@ -67,7 +67,7 @@ class Camera extends GameObject {
 	async tilt() {
 		const animationLength = MathUtils.getRandomInRange(3, 2);
 
-		const fovAnimation = async () =>  {
+		const fovAnimation = async () => {
 			this.#animationCancellationToken.abort();
 			const cancellationToken = new CancellationToken();
 			this.#animationCancellationToken = cancellationToken;
@@ -75,41 +75,42 @@ class Camera extends GameObject {
 			const cancelCondition = () => cancellationToken.isAborted;
 			const [end, endZ] = [85, this.#startZPos * 1.1];
 			await Promise.all([
-				Animations.lerp(fov => this.fov = fov, 3*animationLength, this.fov, end, cancelCondition),
-				Animations.lerp(z => this.#zPos = z, 3*animationLength, this.#zPos, endZ, cancelCondition)
+				Animations.lerp(fov => this.fov = fov, 3 * animationLength, this.fov, end, cancelCondition),
+				Animations.lerp(z => this.#zPos = z, 3 * animationLength, this.#zPos, endZ, cancelCondition)
 			]);
-			
+
 			await Promise.all([
-				Animations.lerp(fov => this.fov = fov, 1*animationLength, this.fov, this.#startFov, cancelCondition),
-				Animations.lerp(z => this.#zPos = z, 1*animationLength, this.#zPos, this.#startZPos, cancelCondition)
+				Animations.lerp(fov => this.fov = fov, 1 * animationLength, this.fov, this.#startFov, cancelCondition),
+				Animations.lerp(z => this.#zPos = z, 1 * animationLength, this.#zPos, this.#startZPos, cancelCondition)
 			]);
 		}
 
 		const angleAnimation = async () => {
 			const startAngle = this.verticalAngle;
-			const deltaRotation = 2+ MathUtils.getRandomInRange(-2, 2);
+			const deltaRotation = 2 + MathUtils.getRandomInRange(-2, 2);
 
 			await Animations.lerp(angle => this.verticalAngle = angle, animationLength, this.verticalAngle, startAngle + deltaRotation);
-			await Animations.lerp(angle => this.verticalAngle = angle, 2* animationLength, this.verticalAngle, startAngle - deltaRotation);
-			await Animations.lerp(angle => this.verticalAngle = angle, animationLength, this.verticalAngle, startAngle);		
+			await Animations.lerp(angle => this.verticalAngle = angle, 2 * animationLength, this.verticalAngle, startAngle - deltaRotation);
+			await Animations.lerp(angle => this.verticalAngle = angle, animationLength, this.verticalAngle, startAngle);
 		}
 
 
 		const tiltAnimation = async () => {
 			this.#animatingPosition = true;
-			const startPos = this.position;			
-			const [startAngle, endAngle] = [0,2 * Math.PI];									
-			const curNoiseX = angle => noise.simplex2(Math.cos(angle), 2* Math.sin(angle)) - 0.5;
-			const curNoiseY = angle => noise.simplex2(2 * Math.cos(angle), Math.sin(angle)) - 0.5;
-			
+			const [startAngle, endAngle] = [0, 2 * Math.PI];
+			const noiseResolution = 1.5;
+			const noiseFactor = 0.1;
+			const curNoiseX = angle => noise.simplex2(Math.cos(angle) * noiseResolution, 2 * Math.sin(angle) * noiseResolution) - 0.5;
+			const curNoiseY = angle => noise.simplex2(2 * Math.cos(angle) * noiseResolution, Math.sin(angle) * noiseResolution) - 0.5;
+
 			await Promise.all([
-				
-				Animations.lerp(angle => this.position = MathUtils.sum(this.#computeCurrentPosition(), MathUtils.mul(0.1, [curNoiseX(angle), curNoiseY(angle), 0])),
-								animationLength, startAngle, endAngle),
+
+				Animations.lerp(angle => this.position = MathUtils.sum(this.#computeCurrentPosition(), MathUtils.mul(noiseFactor, [curNoiseX(angle), curNoiseY(angle), 0])),
+					animationLength, startAngle, endAngle),
 			]);
-			
+
 			this.#animatingPosition = false;
-		}		
+		}
 
 		await Promise.all([tiltAnimation(), fovAnimation()]);
 	}
