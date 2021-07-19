37227e340cc5b30cb76660e97aa148d4555c5d09
fedeAlterio <48481385+fedeAlterio@users.noreply.github.com>
2021-07-19 17:48:35 +0200

    updd

---
+++ app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import path from 'path';
+
+const __dirname = path.resolve();
+const app = express();
+
+app.use('/', express.static(__dirname + '/public'));
+
+app.get("/", (req, res) => {
+    res.sendFile(join(__dirname, 'public', 'index.html'));
+});
+
+
+// PORT
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+   console.log(`Server is running on PORT: ${PORT}`);
+});
